b5d1c2dbf554bce1991c4ebd74068c424d743933
shyrbik <58969166+shyrbik@users.noreply.github.com>
2021-02-14 12:30:14 +0300

    feat: add me code

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+let sortedArray = [];
+let k = 0;
+
+for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++, k++) {
+
+        sortedArray[k] = matrix[i][j];
+    }
+}
+
+return sortedArray.sort(function(a, b) {return a - b; })
 }
