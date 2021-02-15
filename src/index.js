2950a91b6d058b9baef08050513e36b4b98701cf
shyrbik <58969166+shyrbik@users.noreply.github.com>
2021-02-15 12:26:55 +0300

    update

--- src/index.js
+++ src/index.js
@@ -2,20 +2,21 @@ module.exports = function towelSort (matrix) {
 let sortedArray = [];
 let k = 0;
 
-for (let i = 0; i < matrix.length; i++) {
-    console.log("i / 2 =" + i % 2);
+for (let i = 0; i < matrix.length; i++) 
+    {    
     if (i % 2 == 0){
         for (let j = 0; j < matrix[0].length; j++) {
+            if(matrix[i][j] != undefined)
           sortedArray[k] = matrix[i][j];
           k++;
         }
     }
 
     else {
-    for (let j = matrix[0].length-1; j > -1; j--) {
+    for (let j = matrix[i].length-1; j > -1; j--) {
         sortedArray[k] = matrix[i][j];
         k++;
-    }
+        }
     }
 
 }
