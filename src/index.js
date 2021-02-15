3c7c02a2b016c476cb1fda3a80df395f2c666fe0
shyrbik <58969166+shyrbik@users.noreply.github.com>
2021-02-15 12:11:17 +0300

    fix: fix

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
 let sortedArray = [];
 let k = 0;
 
 for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < matrix[0].length; j++, k++) {
-
-        sortedArray[k] = matrix[i][j];
+    console.log("i / 2 =" + i % 2);
+    if (i % 2 == 0){
+        for (let j = 0; j < matrix[0].length; j++) {
+          sortedArray[k] = matrix[i][j];
+          k++;
+        }
     }
+
+    else {
+    for (let j = matrix[0].length-1; j > -1; j--) {
+        sortedArray[k] = matrix[i][j];
+        k++;
+    }
+    }
+
 }
 
-return sortedArray.sort(function(a, b) {return a - b; })
+return sortedArray;
 }
